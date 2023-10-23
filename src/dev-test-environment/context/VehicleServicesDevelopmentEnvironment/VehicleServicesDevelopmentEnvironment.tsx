1867babacced4e74f145ed030326b4b64d8d2ab4
Kain Bryan-Jones <kain@ds.evam.life>
2023-10-23 16:46:10 +0200

    [#12] - refactor the VehicleServicesDevelopmentEnvironment so that it renders its children(the certified app) inside Vehicle Services but not the DevelopmentEnvironment

--- src/dev-test-environment/context/VehicleServicesDevelopmentEnvironment/VehicleServicesDevelopmentEnvironment.tsx
+++ src/dev-test-environment/context/VehicleServicesDevelopmentEnvironment/VehicleServicesDevelopmentEnvironment.tsx
@@ -1,19 +1,19 @@
 import React, {PropsWithChildren} from "react";
 import {ChakraProvider} from "@chakra-ui/react";
 import {NotificationProvider} from "./NotificationProvider";
-import {VehicleServicesNoRender} from "../../components";
+import {EvamApi} from "../../../api/EvamApi";
 
 const VehicleServicesDevelopmentEnvironment: React.FC<PropsWithChildren> = ({children}) => {
 
-    return (
-        <VehicleServicesNoRender>
-                <ChakraProvider>
-                    <NotificationProvider>
-                        {children}
-                    </NotificationProvider>
-                </ChakraProvider>
-        </VehicleServicesNoRender>
-    );
+    //just render the app (this is for if we accidentally leave the VehicleServicesDevelopmentEnvironment component in
+    if (EvamApi.isRunningInVehicleServices) return <>{children}</>;
+
+    return (<ChakraProvider>
+        <NotificationProvider>
+            {children}
+        </NotificationProvider>
+    </ChakraProvider>);
+
 
 };
 
