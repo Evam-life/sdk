3502a241c8a9cc2e05638f81df24d32765d6dc17
Kain Bryan-Jones <kain@ds.evam.life>
2023-12-04 17:33:32 +0100

    [#20] - add lasting notifications

--- src/dev-test-environment/context/VehicleServicesDevelopmentEnvironment/NotificationProvider/NotificationProvider.tsx
+++ src/dev-test-environment/context/VehicleServicesDevelopmentEnvironment/NotificationProvider/NotificationProvider.tsx
@@ -1,11 +1,52 @@
-import React, {PropsWithChildren, useEffect, useState} from "react";
-import {Box, SlideFade, VStack} from "@chakra-ui/react";
+import React, {FC, PropsWithChildren, useCallback, useEffect, useMemo, useState} from "react";
+import {Box, Modal, ModalContent, ModalOverlay, SlideFade, VStack} from "@chakra-ui/react";
 import _ from "lodash";
 import {publish, subscribe, unsubscribe} from "../../../util/EventHelpers";
 import {NotificationComponent, VehicleServicesNoRender} from "../../../components";
-import {EvamEvent} from "../../../../domain";
+import {EvamEvent, NotificationType} from "../../../../domain";
 import {_InternalVehicleServicesNotification} from "../../../../domain/_InternalVehicleServicesNotification";
 
+type LastNotificationComponentProps = {
+    lastingNotification: _InternalVehicleServicesNotification,
+    onTrigger: (uuid: string) => void,
+    onRemove: (notification: _InternalVehicleServicesNotification) => void
+}
+
+const LastNotificationComponent: FC<LastNotificationComponentProps> = ({
+                                                                           lastingNotification,
+                                                                           onTrigger,
+                                                                           onRemove
+                                                                       }) => {
+
+        return <Modal closeOnOverlayClick={false} closeOnEsc={false} isOpen={true}
+                      isCentered
+                      onClose={() => {
+
+                      }}>
+            <ModalOverlay/>
+            <ModalContent>
+                <NotificationComponent notification={lastingNotification} onTrigger={onTrigger} onRemove={onRemove}/>
+            </ModalContent>
+        </Modal>;
+    }
+
+    /**
+     * <ModalHeader>
+     *                     {lastingNotification?.heading}
+     *                 </ModalHeader>
+     *                 <ModalBody>
+     *                     <Text>
+     *                         {lastingNotification?.description}
+     *                     </Text>
+     *                 </ModalBody>
+     *                 <ModalFooter>
+     *                     <HStack spacing={4}>
+     *                         <SecondaryButton/>
+     *                         <PrimaryButton/>
+     *                     </HStack>
+     *                 </ModalFooter>
+     */
+;
 
 const NotificationProvider: React.FC<PropsWithChildren> = ({children}) => {
 
@@ -30,7 +71,7 @@ const NotificationProvider: React.FC<PropsWithChildren> = ({children}) => {
         };
     }, [notifications, setNotifications]);
 
-    const dispatchCallback = (uuid: string) => {
+    const dispatchCallback = useCallback((uuid: string) => {
         publish(EvamEvent.VehicleServicesNotificationCallbackTriggered, uuid);
         const notificationsClone = _.clone(notifications);
         const notificationToRemove = notificationsClone.find((notification) => {
@@ -43,9 +84,9 @@ const NotificationProvider: React.FC<PropsWithChildren> = ({children}) => {
                 setNotifications(notificationsClone);
             }
         }
-    };
+    }, [notifications]);
 
-    const removeNotification = (notification: _InternalVehicleServicesNotification) => {
+    const removeNotification = useCallback((notification: _InternalVehicleServicesNotification) => {
         const notificationsClone = _.clone(notifications);
         const notificationIndex = notificationsClone.indexOf(notification);
 
@@ -53,16 +94,38 @@ const NotificationProvider: React.FC<PropsWithChildren> = ({children}) => {
             notificationsClone.splice(notificationIndex, 1);
             setNotifications(notificationsClone);
         }
-    }
+    }, [notifications]);
+
+    const actionNotifications = useMemo(() => notifications.filter(({notificationType}) => notificationType !== NotificationType.LASTING_HUN, [notifications]), [notifications]);
+
+    const lastingNotification: _InternalVehicleServicesNotification | undefined = useMemo(() => notifications.find(({notificationType}) => notificationType === NotificationType.LASTING_HUN), [notifications]);
+    const changeLastingNotificationToAction = useCallback((lastingNotification: _InternalVehicleServicesNotification) => {
+        const notification = notifications.find((notificationToQuery) => lastingNotification === notificationToQuery);
+        if (notification !== undefined) {
+            const newNotifications = notifications.filter((notificationToFilter) => notificationToFilter !== notification);
+            newNotifications.push({
+                ...notification,
+                notificationType: NotificationType.ACTION_HUN
+            });
+            setNotifications(newNotifications);
+        }
+    }, [notifications]);
 
     return (
         <>
             <VehicleServicesNoRender>
+                {
+                    lastingNotification !== undefined ?
+                        <LastNotificationComponent lastingNotification={lastingNotification}
+                                                   onRemove={changeLastingNotificationToAction}
+                                                   onTrigger={dispatchCallback}/> : null
+                }
                 <Box w={"100vw"} h={"100vh"} position={"absolute"}>
                     <VStack left={"1%"} bottom={"1%"} position={"inherit"}>
-                        {notifications.map((notification, index) => (
+                        {actionNotifications.map((notification, index) => (
                             <SlideFade in={true} offsetY={"20px"} key={index}>
-                                <NotificationComponent notification={notification} onTrigger={dispatchCallback} onRemove={removeNotification}/>
+                                <NotificationComponent notification={notification} onTrigger={dispatchCallback}
+                                                       onRemove={removeNotification}/>
                             </SlideFade>
                         ))}
                     </VStack>
