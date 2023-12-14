49f4436d0efa847ba1ab2e566a33965d053b682d
Kain Bryan-Jones <kain@ds.evam.life>
2023-12-14 09:05:15 +0100

    [#21] - improve AndroidMethod type to handle invalid arg types

--- src/domain/_internal/AndroidMethod.ts
+++ src/domain/_internal/AndroidMethod.ts
@@ -1,7 +1,9 @@
 import {AndroidMethodArgumentMap} from "./AndroidMethodArgumentMap";
 
-type AndroidMethod = keyof AndroidMethodArgumentMap
+type AndroidMethod = {
+    [K in keyof AndroidMethodArgumentMap]: AndroidMethodArgumentMap[K] extends readonly (string | number | boolean)[] ? K : never;
+}[keyof AndroidMethodArgumentMap];
 
 export type {
     AndroidMethod
-}
+};
