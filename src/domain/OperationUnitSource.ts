558d0ac5ef6c48d25d6335b67c7dac06e0d2dbff
aleksandar <aleksandar@evam.life>
2024-03-13 14:42:22 +0100

    Extend Operation with operationUnits and assignedResourceMissionNo

---
+++ src/domain/OperationUnitSource.ts
@@ -0,0 +1,9 @@
+enum OperationUnitSource {
+    /** The unit is provided by Central Services */
+    CLOUD = "CLOUD",
+    /** The unit is provided as part of a call-out from SOS */
+    RAKEL = "RAKEL",
+    UNKNOWN = "UNKNOWN"
+}
+
+export {OperationUnitSource};
