912338e90716f9171d8f4cdeae10ba560d9a5f67
Kain Bryan-Jones <kain@ds.evam.life>
2024-05-03 13:25:07 +0200

    add bearings / speed fields

--- src/data/parsers/vehicle-services/locationParser.ts
+++ src/data/parsers/vehicle-services/locationParser.ts
@@ -4,6 +4,8 @@ import { vehicleServicesDatePayloadParser } from "@/utils/common-parsers";
 const locationParser = z.object({
   latitude: z.number(),
   longitude: z.number(),
+  bearing: z.number().optional(),
+  speed: z.number().optional(),
   timestamp: vehicleServicesDatePayloadParser.optional(),
 });
 
